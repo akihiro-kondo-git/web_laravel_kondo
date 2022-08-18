391d15be66489803bed347c6173863ae75f8d87b
akihiro-kondo-git <akihiro.kondo.git@gmail.com>
2022-08-18 15:45:24 +0900

    fix: テーブル定義の修正

---
+++ Laravel/database/migrations/2022_08_18_041344_create_employee_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('employee_id')->unique();
+            $table->char('family_name', 20);
+            $table->char('first_name', 20);
+            $table->integer('section_id');
+            $table->char('mail', 256);
+            $table->integer('gender_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+};
