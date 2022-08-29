4a97dfcf586a0e51e614a26ff1f2c88e583f131f
akihiro-kondo-git <akihiro.kondo.git@gmail.com>
2022-08-29 11:14:16 +0900

    fix: 「サーバーサイドでの入力チェック」でのバグを3つ未修正

--- Laravel/app/Models/Employee.php
+++ Laravel/app/Models/Employee.php
@@ -11,15 +11,13 @@ use Illuminate\Database\Eloquent\Model;
 use PDO;
 
 //explain: データベース操作メソッド一覧
-// Laravelでのデータ登録:laravel_regist_employee()
-// PDOを利用したデータ更新:pdo_update_employee()
-// PDOを利用しないデータ登録:normal_regist_employee()
-// PDOを利用しない一覧データ表示:normal_get_employee()
-// PDOを利用しない詳細データ表示:noraml_get_info()
+
 // PDOを利用したデータ登録:pdo_regist_employee()
 // PDOを利用した一覧データ表示:pdo_get_employee()
-// PDOを利用した詳細データ表示:pdo_get_info()
-// PDOを利用したデータ削除:pdo_delete_employee()
+// PDOを利用した詳細データ表示:pdo_get_info()※
+// PDOを利用したデータ更新:pdo_update_employee()※
+// PDOを利用したデータ削除:pdo_delete_employee()※
+// Laravelでのデータ登録:laravel_regist_employee()
 
 
 class Employee extends Model
@@ -29,139 +27,6 @@ class Employee extends Model
     public $timestamps = false;
     public $incrementing = true;
 
-//--------------------------------------------------------------------------------------------------//
-//-----------------------------------Laravelでのデータベース操作-------------------------------------//
-//--------------------------------------------------------------------------------------------------//
-
-    public static function laravel_regist_employee()
-    {
-
-        $employee = new \App\Models\Employee;
-
-        //explain: データの取得
-        $employee_id = $_POST['employee_id'];
-        $family_name = $POST['family_name'];
-        $first_name = $_POST['first_name'];
-        $section_id = $_POST['section_id'];
-        $mail_address = $_POST['mail_address'];
-        $gender_id = $_POST['gender_id'];
-
-        //$current_id = $employee->select('id')->get();
-
-        //$employee->id = 'defalut';
-        $employee->employee_id = $employee_id;
-        $employee->family_name = $family_name;
-        $employee->first_name = $first_name;
-        $employee->section_id = $section_id;
-        $employee->mail = $mail_address;
-        $employee->gender_id = $gender_id;
-        $employee->save();
-        Message::setMessage("データを登録しました");
-    }
-
-//--------------------------------------------------------------------------------------------------//
-//---------------------------------PDOを利用しないデータベース操作------------------------------------//
-//--------------------------------------------------------------------------------------------------//
-
-    //explain: Employeeデータ登録メソッド
-    public static function normal_regist_employee()
-    {try {
-
-        //explain: データの取得
-        $employee_id = $_POST['employee_id'];
-        $family_name = $_POST['family_name'];
-        $first_name = $_POST['first_name'];
-        $section_id = $_POST['section_id'];
-        $mail_address = $_POST['mail_address'];
-        $gender_id = $_POST['gender_id'];
-
-        //explain: データベースへの接続
-        $link = pg_connect("host=localhost dbname=company_directory user=homestead password=secret");
-        if (!$link) {die('接続失敗です。' . pg_last_error());}
-
-        //explain: テーブルへの登録
-        $sql = "INSERT INTO employee VALUES (default, '$employee_id', '$family_name', '$first_name', $section_id, '$mail_address', $gender_id)";
-        pg_query($sql);
-        Message::setMessage("データを登録しました");
-
-    } catch (SQLException $e) {
-        Message::setMessage("データ登録に失敗しました");
-
-    } finally {
-        //explain: データベースの切断
-        $close_flag = pg_close($link);
-        if (!$close_flag) {print('切断に失敗しました。<br>');}
-    }
-    }
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //explain　Employeeデータ表示メソッド
-    public static function normal_get_employee()
-    {try {
-
-        //explain: データベースへの接続
-        $link = pg_connect("host=localhost dbname=company_directory user=homestead password=secret");
-        if (!$link) {die('接続失敗です。' . pg_last_error());}
-
-        //explain: テーブルデータの取得
-        $result = pg_query("SELECT * FROM employee");
-
-        //explain: 取得したデータを配列に格納
-        for ($i = 0; $i < pg_num_rows($result); $i++) {
-            $row = pg_fetch_array($result, null, PGSQL_ASSOC);
-            $employee_data[$i][1] = $row['employee_id'];
-            $employee_data[$i][2] = $row['family_name'];
-            $employee_data[$i][3] = $row['first_name'];
-            $employee_data[$i][4] = $row['section_id'];
-            $employee_data[$i][5] = $row['mail'];
-            $employee_data[$i][6] = $row['gender_id'];
-        }
-    } catch (SQLException $e) {
-        echo $e;
-    } finally {
-        //explain: データベースの切断
-        $close_flag = pg_close($link);
-        if (!$close_flag) {print('切断に失敗しました。<br>');}
-    }
-
-        return $employee_data;
-
-    }
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-//explain　Employeeデータ表示メソッド
-    public static function normal_get_info($id)
-    {try {
-        $employee_data = array();
-        //explain: データベースへの接続
-        $link = pg_connect("host=localhost dbname=company_directory user=homestead password=secret");
-        if (!$link) {die('接続失敗です。' . pg_last_error());}
-
-        //explain: テーブルデータの取得
-        $sql = "SELECT * FROM employee WHERE employee_id = '$id'";
-        $result = pg_query($sql);
-
-        //explain: 取得したデータを配列に格納
-        $row = pg_fetch_array($result, null, PGSQL_ASSOC);
-        $employee_data[1] = $row['employee_id'];
-        $employee_data[2] = $row['family_name'];
-        $employee_data[3] = $row['first_name'];
-        $employee_data[4] = $row['section_id'];
-        $employee_data[5] = $row['mail'];
-        $employee_data[6] = $row['gender_id'];
-
-        //explain: エラーメッセージの出力
-    } catch (SQLException $e) {
-        echo $e;
-    } finally {
-        //explain: データベースの切断
-        $close_flag = pg_close($link);
-        if (!$close_flag) {print('切断に失敗しました。<br>');}
-    }
-
-        return $employee_data;
-
-    }
 
 //--------------------------------------------------------------------------------------------------//
 //-----------------------------------PDOを利用するデータベース操作------------------------------------//
@@ -223,6 +88,8 @@ class Employee extends Model
 
     ////////////////////////////////////////////////////////////////////////////////////////////
 
+//message: 今後修正してください
+
     //explain　PDOによるデータ表示メソッド
     public static function pdo_get_employee()
     {try {
@@ -258,97 +125,103 @@ class Employee extends Model
     }
     //////////////////////////////////////////////////////////////////////////////////////////////
 
+    //message: 今後修正してください
+
     //explain: PDOによる詳細データ表示メソッド
-    public static function pdo_get_info($id)
-    {try {
-        //explain: データベースへの接続
-        $database = 'pgsql:dbname=company_directory; host=localhost; port=5432;';
-        $user = 'homestead';
-        $password = 'secret';
-        $pdo = new PDO($database, $user, $password);
+    public static function pdo_get_info($id){
+    // try {
+    //     //explain: データベースへの接続
+    //     $database = 'pgsql:dbname=company_directory; host=localhost; port=5432;';
+    //     $user = 'homestead';
+    //     $password = 'secret';
+    //     $pdo = new PDO($database, $user, $password);
 
-        //explain: トランザクション開始
-        $pdo->beginTransaction();
+    //     //explain: トランザクション開始
+    //     $pdo->beginTransaction();
 
-        //explain: SQL文のセット
-        $stmt = $pdo->prepare("SELECT * FROM employee WHERE employee_id = :employee_id ");
-        $stmt->bindParam(':employee_id', $id, PDO::PARAM_INT);
+    //     //explain: SQL文のセット
+    //     $stmt = $pdo->prepare("SELECT * FROM employee WHERE employee_id = :employee_id ");
+    //     $stmt->bindParam(':employee_id', $id, PDO::PARAM_INT);
 
-        //explain: SQLの実行
-        $stmt->execute();
+    //     //explain: SQLの実行
+    //     $stmt->execute();
 
-        //explain: SQLの結果をまとめて配列に格納
-        $employee_data = $stmt->fetch();
+    //     //explain: SQLの結果をまとめて配列に格納
+    //     $employee_data = $stmt->fetch();
 
-        //explain: エラーメッセージを出力とロールバック
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-        $pdo->rollBack();
+    //     //explain: エラーメッセージを出力とロールバック
+    // } catch (PDOException $e) {
+    //     echo $e->getMessage();
+    //     $pdo->rollBack();
 
-        //explain: データベースの切断
-    } finally {
-        $pdo = null;
-    }
+    //     //explain: データベースの切断
+    // } finally {
+    //     $pdo = null;
+    // }
 
-        return $employee_data;
+    //     return $employee_data;
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////
+
+//message: 今後修正してください
 
     //explain: PDOによるデータ更新メソッド
     public static function pdo_update_employee()
     {
 
-        //explain: 入力データの取得
-        $employee_id = $_POST['employee_id'];
-        $family_name = $_POST['family_name'];
-        $first_name = $_POST['first_name'];
-        $section_id = $_POST['section_id'];
-        $mail = $_POST['mail_address'];
-        $gender_id = $_POST['gender_id'];
+        // //explain: 入力データの取得
+        // $employee_id = $_POST['employee_id'];
+        // $family_name = $_POST['family_name'];
+        // $first_name = $_POST['first_name'];
+        // $section_id = $_POST['section_id'];
+        // $mail = $_POST['mail_address'];
+        // $gender_id = $_POST['gender_id'];
 
-        try {
-            //explain: データベースへの接続
-            $database = 'pgsql:dbname=company_directory; host=localhost; port=5432;';
-            $user = 'homestead';
-            $password = 'secret';
-            $pdo = new PDO($database, $user, $password);
+        // try {
+        //     //explain: データベースへの接続
+        //     $database = 'pgsql:dbname=company_directory; host=localhost; port=5432;';
+        //     $user = 'homestead';
+        //     $password = 'secret';
+        //     $pdo = new PDO($database, $user, $password);
 
-            //explain: トランザクション開始
-            $pdo->beginTransaction();
+        //     //explain: トランザクション開始
+        //     $pdo->beginTransaction();
 
-            //explain: テーブルへの登録
-            $stmt = $pdo->prepare('UPDATE employee SET family_name = :family_name,first_name = :first_name, section_id = :section_id, mail = :mail,gender_id= :gender_id WHERE employee_id = :employee_id');
+        //     //explain: テーブルへの登録
+        //     $stmt = $pdo->prepare('UPDATE employee SET family_name = :family_name,first_name = :first_name, section_id = :section_id, mail = :mail,gender_id= :gender_id WHERE employee_id = :employee_id');
 
-            //explain: 入力データの挿入
-            $stmt->bindParam(':employee_id', $employee_id, PDO::PARAM_INT);
-            $stmt->bindParam(':family_name', $family_name, PDO::PARAM_STR);
-            $stmt->bindParam(':first_name', $first_name, PDO::PARAM_STR);
-            $stmt->bindParam(':section_id', $section_id, PDO::PARAM_INT);
-            $stmt->bindParam(':mail', $mail, PDO::PARAM_STR);
-            $stmt->bindParam(':gender_id', $gender_id, PDO::PARAM_INT);
+        //     //explain: 入力データの挿入
+        //     $stmt->bindParam(':employee_id', $employee_id, PDO::PARAM_INT);
+        //     $stmt->bindParam(':family_name', $family_name, PDO::PARAM_STR);
+        //     $stmt->bindParam(':first_name', $first_name, PDO::PARAM_STR);
+        //     $stmt->bindParam(':section_id', $section_id, PDO::PARAM_INT);
+        //     $stmt->bindParam(':mail', $mail, PDO::PARAM_STR);
+        //     $stmt->bindParam(':gender_id', $gender_id, PDO::PARAM_INT);
 
-            //explain: SQLの実行
-            $res = $stmt->execute();
+        //     //explain: SQLの実行
+        //     $res = $stmt->execute();
 
-            //explain: コミット
-            if ($res) {$pdo->commit();}
-            Message::setMessage("データを更新しました");
+        //     //explain: コミット
+        //     if ($res) {$pdo->commit();}
+        //     Message::setMessage("データを更新しました");
 
-            //explain: エラーメッセージを出力とロールバック
-        } catch (\PDOException$e) {
-            $pdo->rollBack();
-            Message::setMessage("データ更新に失敗しました");
+        //     //explain: エラーメッセージを出力とロールバック
+        // } catch (\PDOException$e) {
+        //     $pdo->rollBack();
+        //     Message::setMessage("データ更新に失敗しました");
 
-            //explain: データベースの切断
-        } finally {
-            $pdo = null;
+        //     //explain: データベースの切断
+        // } finally {
+        //     $pdo = null;
 
-        }
+        // }
 
     }
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+//message: 今後修正してください
 
     //explain: PDOによるデータ削除メソッド
     public static function pdo_delete_employee($id){
@@ -384,5 +257,37 @@ class Employee extends Model
     
     }
 //}
+
+
+
+//--------------------------------------------------------------------------------------------------//
+//-----------------------------------Laravelでのデータベース操作-------------------------------------//
+//--------------------------------------------------------------------------------------------------//
+
+public static function laravel_regist_employee()
+{
+
+    $employee = new \App\Models\Employee;
+
+    //explain: データの取得
+    $employee_id = $_POST['employee_id'];
+    $family_name = $POST['family_name'];
+    $first_name = $_POST['first_name'];
+    $section_id = $_POST['section_id'];
+    $mail_address = $_POST['mail_address'];
+    $gender_id = $_POST['gender_id'];
+
+    //$current_id = $employee->select('id')->get();
+
+    //$employee->id = 'defalut';
+    $employee->employee_id = $employee_id;
+    $employee->family_name = $family_name;
+    $employee->first_name = $first_name;
+    $employee->section_id = $section_id;
+    $employee->mail = $mail_address;
+    $employee->gender_id = $gender_id;
+    $employee->save();
+    Message::setMessage("データを登録しました");
+}
 
 }
--- Laravel/resources/views/list-employee.blade.php
+++ Laravel/resources/views/list-employee.blade.php
@@ -2,115 +2,12 @@
 //--------------------------------------------------------------------------------------------------//
 //------------------------------------------社員一覧画面---------------------------------------------//
 //--------------------------------------------------------------------------------------------------//
-use App\Http\Controllers\EmployeeData\EmployeeAll;
-EmployeeAll::setArray();
+
+
+
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <link href="{{ asset('css/list.css') }}" rel="stylesheet">
-    <title>社員一覧画面</title>
-</head>
-
-<body>
-<table>
-            <tr>
-                <td>&nbsp;社員ID</td>
-                <td>&emsp;社員名</td>
-                <td>&emsp;所属セクション</td>
-                <td>&emsp;メールアドレス</td>
-                <td>&emsp;性別</td>
-            </tr>
-
-            <!-- explain: 社員一覧を表示 -->
-            <?php for ($i = 0; $i < EmployeeAll::$count; $i++):?>
-
-	        <tr>
-	        <td>
-	        <form action = http://192.168.56.56/refer-employee name = employee method = GET>
-	            <button type = submit class = link-style name = employee value = <?php echo EmployeeAll::$employee_id[$i] ?>>
-	            <?php echo EmployeeAll::$employee_id[$i] ?>
-                </button>
-            </form>
-            </td>
-	        <td>&emsp;<?php echo EmployeeAll::$family_name[$i] ?><?php echo EmployeeAll::$first_name[$i] ?></td>
-	        <td>&emsp;<?php echo EmployeeAll::$section_name[$i] ?></td>
-	        <td>&emsp;<?php echo EmployeeAll::$mail_address[$i] ?></td>
-	        <td>&emsp;<?php echo EmployeeAll::$gender_name[$i] ?></td>
-	        </tr>
-
-	        <?php endfor;?>
-
-        </table>
-    <br>
-    <a href = "{{route('regist-employee')}}">社員登録画面</a></br>
-    <a href = "{{route('menu')}}">メニュー画面</a>
-
-</body>
-</html>
 
 
 
 
-
-<!-- 以下php -->
-
-
-
-
-
-<?php
-
-// use App\Models\Employee;
-
-// //method: 社員一覧表示メソッド
-
-// //explain: データベースからデータを取得
-// //(PDOを利用しない場合:「get_employee()」メソッドを利用)
-
-//     $employee = new Employee();
-//     $employee_data = $employee->pdo_get_employee();
-
-// //explain: 取得したデータを各変数に代入
-//     for ($i = 0; $i < count($employee_data); $i++) {
-//         $employee_id = $employee_data[$i][1];
-//         $family_name = $employee_data[$i][2];
-//         $first_name = $employee_data[$i][3];
-//         $section_id = $employee_data[$i][4];
-//         $mail_address = $employee_data[$i][5];
-//         $gender_id = $employee_data[$i][6];
-
-//         //explain: 所属セクション名とセクションIDを合致
-//         if ($section_id == 1) {
-//             $section_name = "シス開";
-//         } else if ($section_id == 2) {
-//             $section_name = "グロカル";
-//         } else {
-//             $section_name = "ビジソル";
-//         }
-
-//         //explain: 性別名と性別IDを合致
-//         if ($gender_id == 1) {
-//             $gender_name = "男性";
-//         } else {
-//             $gender_name = "女性";
-//         }
-
-// //explain 社員データの表示 origin
-// echo '<tr>';
-// echo "<td><form action = http://192.168.56.56/refer-employee
-//     name = employee method = GET>
-//     <button type = submit class = link-style name = employee
-// value = $employee_id>$employee_id</button></form></td>
-// <td>&emsp;$family_name$first_name</td>
-// <td>&emsp;$section_name</td>
-// <td>&emsp;$mail_address</td>
-// <td>&emsp;$gender_name</td>";
-// echo '</tr>';
-
-//explain 社員データの表示
-
-// }
-//}
-//?>
 
